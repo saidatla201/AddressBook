eca821c18dd8e00456b67fb9bdffc5d5f368f3f3
saidatla201 <saidatla201@gmail.com>
2023-02-23 22:12:20 +0530

    Adding adress for a Contact

---
+++ Contact.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public  class Contact
+    {
+        public void contact()
+        {
+            Console.Write("Enter the First name :");
+            string Firstname = Console.ReadLine();
+            Console.Write("Enter the Last name :");
+            string Lastname = Console.ReadLine();
+            Console.Write("Enter the Address :");
+            string Address = Console.ReadLine();
+            Console.Write("Enter the State name :");
+            string Statename = Console.ReadLine();
+            Console.Write("Enter the District name :");
+            string District = Console.ReadLine();
+            Console.Write("Enter the Zip Code :");
+            int Zipcode = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter your Phone Number :");
+            long  PhoneNum = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+        }
+    }
+}
